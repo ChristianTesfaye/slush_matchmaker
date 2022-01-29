026f70fb279e2e02522340d5ac5a62b094cb30c5
Christian Kassahun <christes7@gmail.com>
2022-01-30 02:55:38 +0300

    Added null safety for cookie parser

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,10 +1,12 @@
-const parseCookie = (str: string) =>
-  str
+const parseCookie = (str: string = "") =>
+{
+  var str = str || ""
+  return str 
   .split(';')
   .map(v => v.split('='))
   .reduce((acc, v) => {
     acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
     return acc;
   }, {});
-
+}
 export default parseCookie
